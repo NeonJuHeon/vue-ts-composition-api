a110c1ef9ca862740ea43561fffba3e625b78a9e
JuHeonHa <neon.ha@devercorp.com>
2022-11-03 13:24:42 +0900

    initial commit

---
+++ src/views/SimpleCounter.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>Counter</h1>
+	<div>{{ count }}</div>
+	<div>
+		<button @click="onClickAdd">+</button>
+		<button @click="onClickSubtract">-</button>
+	</div>
+	<!--<div class="home">Home</div>-->
+</template>
+
+<script lang="ts">
+	import { defineComponent, ref } from 'vue';
+
+	export default defineComponent({
+		setup() {
+			const count = ref<number>(0);
+			const onClickAdd = () => {
+				count.value++;
+			};
+			const onClickSubtract = () => {
+				count.value--;
+			};
+			return {
+				count,
+				onClickAdd,
+				onClickSubtract,
+			};
+		},
+	});
+</script>
